62d66d7b930f0f83d616264d3546ca07d03bffde
Mika Tammi <mikatammi@gmail.com>
2011-12-16 15:37:35 +0200

    Improved scanner and added multithreading option to boost

--- src/libdeck/db/scanner/Scanner.cpp
+++ src/libdeck/db/scanner/Scanner.cpp
@@ -4,7 +4,65 @@ namespace deck {
 namespace db {
 namespace scanner {
 
-Scanner::Scanner()
+Scanner::Scanner(const std::string &path) :
+    filelist_(),
+    mutex_(),
+    main_thread_(),
+    main_future_(),
+    run_main_thread_(false),
+    resultlist_()
+{
+}
+
+Scanner::~Scanner()
+{
+}
+
+void Scanner::start()
+{
+    boost::packaged_task <ScannerTrackInfoList>
+            pt(ScannerTrackInfoList main_thread);
+    //main_future_ = pt.get_future();
+}
+
+void Scanner::stop()
+{
+}
+
+void Scanner::wait()
+{
+    // Wait for main future to be ready
+    main_future_.wait();
+}
+
+bool Scanner::isReady() const
+{
+    // Return main future readyness
+    return main_future_.is_ready();
+}
+
+ScannerTrackInfoList Scanner::get()
+{
+    if(!isReady() /* && sth else TODO: Verify process started */)
+    {
+        wait();
+    }
+    else
+    {
+        // Some sort of fail.
+        // Returning empty list.
+        return ScannerTrackInfoList();
+    }
+
+    // Returns resulting list
+    return main_future_.get();
+}
+
+ScannerTrackInfoList Scanner::main_thread()
+{
+}
+
+void Scanner::scan_gather_files()
 {
 }
 
--- src/libdeck/db/scanner/Scanner.hpp
+++ src/libdeck/db/scanner/Scanner.hpp
@@ -1,14 +1,67 @@
 #ifndef SCANNER_HPP
 #define SCANNER_HPP
 
+#include "ScannerTrackInfo.hpp"
+
+#include <string>
+#include <list>
+#include <queue>
+
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/future.hpp>
+
 namespace deck {
 namespace db {
 namespace scanner {
 
+typedef std::list <boost::shared_ptr <ScannerTrackInfo> >
+    ScannerTrackInfoList;
+
+/// Scanner which scans particular subdirectory recursively.
 class Scanner
 {
 public:
-    Scanner();
+    /// Constructor for scanner.
+    /// @param path Path to scan.
+    explicit Scanner(const std::string& path);
+
+    /// Destructor
+    ~Scanner();
+
+    /// Start scan job thread.
+    void start();
+
+    /// Stop scan job thread.
+    void stop();
+
+    /// Wait for execution of thread
+    void wait();
+
+    /// Checks whether scanner job is ready or not.
+    /// @return True if ready.
+    bool isReady() const;
+
+    /// Get result. Waits if process not ready.
+    /// @return Resulting list from scan
+    ScannerTrackInfoList get();
+
+private:
+    ScannerTrackInfoList main_thread();
+    void scan_gather_files();
+
+    std::queue <std::string> filelist_;
+
+    boost::mutex mutex_;
+
+    boost::thread main_thread_;
+    boost::unique_future<ScannerTrackInfoList>
+        main_future_;
+
+    bool run_main_thread_;
+
+    ScannerTrackInfoList resultlist_;
 };
 
 }
