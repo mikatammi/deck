b737ddee79bdc8ed9a44ffb15a098286a8d4038c
Mika Tammi <mikatammi@gmail.com>
2012-01-01 13:30:11 +0200

    Improved functionality of MainPlayer class

--- src/libdeck/MainPlayer.cpp
+++ src/libdeck/MainPlayer.cpp
@@ -1,8 +1,88 @@
 #include "MainPlayer.hpp"
+#include "audio/AudioManager.hpp"
+#include "connection/ConnectionManager.hpp"
+#include "db/DatabaseManager.hpp"
+#include "db/PlayList.hpp"
+
+#include <boost/assert.hpp>
 
 namespace deck
 {
-MainPlayer::MainPlayer()
+
+MainPlayer::MainPlayer() :
+    state_(STOPPED),
+    audiomanager_(0),
+    connectionmanager_(0),
+    databasemanager_(0),
+    activeplaylist_(0)
 {
+    // Create new manager objects for audio, connections and database
+    audiomanager_ = new audio::AudioManager();
+    connectionmanager_ = new connection::ConnectionManager();
+    databasemanager_ = new db::DatabaseManager();
+    activeplaylist_ = new db::PlayList();
+
+    BOOST_ASSERT(audiomanager_ != 0);
+    BOOST_ASSERT(connectionmanager_ != 0);
+    BOOST_ASSERT(databasemanager_ != 0);
+    BOOST_ASSERT(activeplaylist_ != 0);
 }
+
+void MainPlayer::play()
+{
+    state_ = PLAYING;
+
+    // TODO: Use audio manager to play
+}
+
+void MainPlayer::pause()
+{
+    if(state_ == PLAYING)
+    {
+        state_ = PAUSED;
+    }
+    else if(state_ == PAUSED)
+    {
+        state_ = PLAYING;
+    }
+
+    // TODO: Use audio manager to pause
+}
+
+void MainPlayer::stop()
+{
+    if(state_ == PLAYING || state_ == PAUSED)
+    {
+        state_ = STOPPED;
+    }
+
+    // TODO: Use audio manager to stop
+}
+
+void MainPlayer::prev()
+{
+    // TODO
+}
+
+void MainPlayer::next()
+{
+    // TODO
+}
+
+MainPlayer::PlayState MainPlayer::getState() const
+{
+    return state_;
+}
+
+db::PlayList * MainPlayer::getActivePlayList()
+{
+    return activeplaylist_;
+}
+
+void MainPlayer::setActivePlayList(db::PlayList *playlist)
+{
+    activeplaylist_ = playlist;
+    BOOST_ASSERT(activeplaylist_ != 0);
+}
+
 }
--- src/libdeck/MainPlayer.hpp
+++ src/libdeck/MainPlayer.hpp
@@ -1,8 +1,23 @@
 #ifndef MAINPLAYER_HPP
 #define MAINPLAYER_HPP
 
-namespace deck
-{
+namespace deck {
+
+// Forward declaration for AudioManager
+namespace audio {
+class AudioManager;
+}
+
+// Forward declaration for ConnectionManager
+namespace connection {
+class ConnectionManager;
+}
+
+// Forward declaration for DatabaseManager and PlayList
+namespace db {
+class DatabaseManager;
+class PlayList;
+}
 
 /// Main Player class handles all abstraction to user interface.
 /// Subsystems handle different parts of the program and are
@@ -10,8 +25,53 @@ namespace deck
 class MainPlayer
 {
 public:
+    enum PlayState
+    {
+        PLAYING, PAUSED, STOPPED
+    };
+
     /// Constructs Main Player
     explicit MainPlayer();
+
+    /// Start playing if player is stopped or paused. If already playing,
+    /// song is started from beginning.
+    void play();
+
+    /// Pause current song. If there is no song playing, does nothing.
+    void pause();
+
+    /// Stops current song. If there is no song playing, does nothing.
+    void stop();
+
+    /// Moves to previous song. If there is no previous song, does nothing.
+    void prev();
+
+    /// Moves to next song. If there is no next song, stops playing.
+    void next();
+
+    /// Get status of playing
+    /// @return Enum-value of player status (PLAYING, PAUSED or STOPPED)
+    PlayState getState() const;
+
+    /// Get active playlist
+    /// @return Pointer to active playlist
+    db::PlayList * getActivePlayList();
+
+    /// Set active playlist
+    /// @param playlist Pointer to playlist
+    void setActivePlayList(db::PlayList * playlist);
+
+private:
+    MainPlayer(const MainPlayer&);
+    MainPlayer& operator= (const MainPlayer&);
+
+    PlayState state_;
+
+    audio::AudioManager * audiomanager_;
+    connection::ConnectionManager * connectionmanager_;
+    db::DatabaseManager * databasemanager_;
+    db::PlayList * activeplaylist_;
+
 };
 }
 
