e130fc1f733ff374e170956df57db54727015ba7
Mika Tammi <mikatammi@gmail.com>
2012-01-02 00:00:52 +0200

    Added ApplicationName into Qt core application object

--- src/deckqt/main.cpp
+++ src/deckqt/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char* argv[])
     // Create application
     QApplication app(argc, argv);
 
+    app.setApplicationName("Deck");
+
     // Create main window
     MainWindow mainWindow;
     mainWindow.show();
