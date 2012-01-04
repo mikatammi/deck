21ab4db2b06ea61f7e1194a9c1bb6f58cc7c6afe
Mika Tammi <mikatammi@gmail.com>
2012-01-05 00:56:44 +0200

    Improvements to OptionsDialog: Added layout some connects

--- src/deckqt/OptionsDialog.cpp
+++ src/deckqt/OptionsDialog.cpp
@@ -1,14 +1,61 @@
 #include "OptionsDialog.hpp"
 #include "ui_OptionsDialog.h"
 
-OptionsDialog::OptionsDialog(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::OptionsDialog)
+#include "GenericOptionsWidget.hpp"
+#include "DatabaseOptionsWidget.hpp"
+#include "PeerOptionsWidget.hpp"
+
+#include "MainPlayer.hpp"
+
+#include <QPushButton>
+
+OptionsDialog::OptionsDialog(deck::MainPlayer* mainplayer, QWidget *parent) :
+    QDialog(parent),
+    ui_(new Ui::OptionsDialog),
+    mainplayer_(mainplayer),
+    genericoptions_(0),
+    databaseoptions_(0),
+    peeroptions_(0)
 {
-    ui->setupUi(this);
+    ui_->setupUi(this);
+
+    // Allocate tab widgets
+    genericoptions_ = new GenericOptionsWidget(this);
+    databaseoptions_ = new DatabaseOptionsWidget(this);
+    peeroptions_ = new PeerOptionsWidget(this);
+
+    // Add new widgets as tabs
+    ui_->tabWidget->addTab(genericoptions_, tr("Generic"));
+    ui_->tabWidget->addTab(databaseoptions_, tr("Database"));
+    ui_->tabWidget->addTab(peeroptions_, tr("Peers"));
+
+    // Connect OK and Cancel buttons
+    connect(ui_->buttonBox, SIGNAL(accepted()), SLOT(onOK()));
+    connect(ui_->buttonBox, SIGNAL(rejected()), SLOT(reject()));
+
+    // Connect Apply button
+    QPushButton * btnApply = ui_->buttonBox->button(QDialogButtonBox::Apply);
+    connect(btnApply, SIGNAL(clicked()), SLOT(onApply()));
 }
 
 OptionsDialog::~OptionsDialog()
 {
-    delete ui;
+    delete genericoptions_;
+    delete databaseoptions_;
+    delete peeroptions_;
+
+    delete ui_;
+}
+
+void OptionsDialog::onOK()
+{
+    // Apply settings
+    onApply();
+
+    accept();
+}
+
+void OptionsDialog::onApply()
+{
+    // TODO: Save settings
 }
--- src/deckqt/OptionsDialog.hpp
+++ src/deckqt/OptionsDialog.hpp
@@ -1,22 +1,45 @@
 #ifndef OPTIONSDIALOG_HPP
 #define OPTIONSDIALOG_HPP
 
-#include <QWidget>
+#include <QDialog>
+
+namespace deck {
+class MainPlayer;
+}
 
 namespace Ui {
 class OptionsDialog;
 }
 
-class OptionsDialog : public QWidget
+class GenericOptionsWidget;
+class DatabaseOptionsWidget;
+class PeerOptionsWidget;
+
+class OptionsDialog : public QDialog
 {
     Q_OBJECT
     
 public:
-    explicit OptionsDialog(QWidget *parent = 0);
+    /// Constructor for OptionsDialog
+    /// @param mainplayer Pointer to MainPlayer-class
+    /// @param parent Parent QWidget
+    explicit OptionsDialog(deck::MainPlayer* mainplayer, QWidget *parent = 0);
     ~OptionsDialog();
-    
+
+private slots:
+    /// Activated when user clicks Ok
+    void onOK();
+
+    /// Activated when user clicks Apply (or Ok)
+    void onApply();
+
 private:
-    Ui::OptionsDialog *ui;
+    Ui::OptionsDialog* ui_;
+    deck::MainPlayer* mainplayer_;
+
+    GenericOptionsWidget* genericoptions_;
+    DatabaseOptionsWidget* databaseoptions_;
+    PeerOptionsWidget* peeroptions_;
 };
 
 #endif // OPTIONSDIALOG_HPP
