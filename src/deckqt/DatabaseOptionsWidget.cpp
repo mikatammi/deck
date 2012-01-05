3bdb2ea3f23e90d2d29c0e56b6a47e55d1e5d9d5
Mika Tammi <mikatammi@gmail.com>
2012-01-05 03:22:25 +0200

    Added parameter to DatabaseOptionsWidget constructor to set selected directories.

--- src/deckqt/DatabaseOptionsWidget.cpp
+++ src/deckqt/DatabaseOptionsWidget.cpp
@@ -3,18 +3,20 @@
 
 #include "FileSystemModel.hpp"
 
-DatabaseOptionsWidget::DatabaseOptionsWidget(QWidget *parent) :
+DatabaseOptionsWidget::DatabaseOptionsWidget(QSet <QString> selected_dirs,
+                                             QWidget *parent) :
     QWidget(parent),
     ui_(new Ui::DatabaseOptionsWidget)
 {
     ui_->setupUi(this);
 
-    FileSystemModel* model = new FileSystemModel(QSet <QString> (), this);
-
-    ui_->treeView->setModel(model);
+    // Create filesystem model for treeview to show file structure
+    FileSystemModel* fsmodel = new FileSystemModel(selected_dirs, this);
+    ui_->treeView->setModel(fsmodel);
 }
 
 DatabaseOptionsWidget::~DatabaseOptionsWidget()
 {
     delete ui_;
+    ui_ = 0;
 }
--- src/deckqt/DatabaseOptionsWidget.hpp
+++ src/deckqt/DatabaseOptionsWidget.hpp
@@ -2,17 +2,24 @@
 #define DATABASEOPTIONSWIDGET_HPP
 
 #include <QWidget>
+#include <QSet>
+#include <QString>
 
 namespace Ui {
 class DatabaseOptionsWidget;
 }
 
+/// Database Options Tab for use in OptionsDialog
 class DatabaseOptionsWidget : public QWidget
 {
     Q_OBJECT
     
 public:
-    explicit DatabaseOptionsWidget(QWidget *parent = 0);
+    /// Constructor for DatabaseOptionsWidget
+    /// @param selected_dirs Selected directories in tree view
+    /// @param parent Parent Widget
+    explicit DatabaseOptionsWidget(QSet <QString> selected_dirs,
+                                   QWidget *parent = 0);
     ~DatabaseOptionsWidget();
     
 private:
