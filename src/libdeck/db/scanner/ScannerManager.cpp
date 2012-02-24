1443bdb13b54de21a3590377304357aaaf060d7b
Your Name <mikatammi@gmail.com>
2012-02-24 17:02:36 +0200

    Implemented part of ScannerManager

--- src/libdeck/db/scanner/ScannerManager.cpp
+++ src/libdeck/db/scanner/ScannerManager.cpp
@@ -1,13 +1,68 @@
 #include "ScannerManager.hpp"
 #include "Scanner.hpp"
 
+#include <boost/foreach.hpp>
+
 namespace deck {
 namespace db {
 namespace scanner {
 
 ScannerManager::ScannerManager() :
+    scan_directories_(),
     scanners_()
 {
+}
+
+ScannerManager::~ScannerManager()
+{
+    // Stop and delete scanners on scannermanager destruction
+    stopAndDeleteScanners();
+}
+
+void ScannerManager::setScanDirectories(const std::list <std::string>&
+                                        directories)
+{
+    // Stop existing scanners
+    stopAndDeleteScanners();
+
+    // Set directories to scan
+    scan_directories_ = directories;
+
+    // Iterate through every directory
+    BOOST_FOREACH(std::string dir, scan_directories_)
+    {
+        // Create and start scanner for each directory
+        Scanner* new_scanner = new Scanner(dir);
+        new_scanner->start();
+
+        // Add scanners to scanner list
+        scanners_.push_back(new_scanner);
+    }
+}
+
+std::list <std::string> ScannerManager::getScanDirectories() const
+{
+    // Returns list of directories currently being scanned
+    return scan_directories_;
+}
+
+void ScannerManager::stopAndDeleteScanners()
+{
+    // Iterate through every scanner
+    for(std::list <Scanner*> ::iterator it = scanners_.begin();
+        it != scanners_.end();
+        ++it)
+    {
+        // Stop every scanner
+        (*it)->stop();
+
+        // Delete every scanner
+        delete (*it);
+        (*it) = 0;
+    }
+
+    // Clear scanner list
+    scanners_.clear();
 }
 
 }
--- src/libdeck/db/scanner/ScannerManager.hpp
+++ src/libdeck/db/scanner/ScannerManager.hpp
@@ -1,6 +1,7 @@
 #ifndef SCANNERMANAGER_HPP
 #define SCANNERMANAGER_HPP
 
+#include <string>
 #include <list>
 
 namespace deck {
@@ -9,16 +10,31 @@ namespace scanner {
 
 class Scanner;
 
+/// Class which watches set directories for changes
 class ScannerManager
 {
 public:
-    ScannerManager();
+    /// Default constructor
+    explicit ScannerManager();
+
+    /// Destructor
+    ~ScannerManager();
+
+    /// Set directories for ScannerManager to watch
+    /// @param directories List of directory strings
+    void setScanDirectories(const std::list <std::string>& directories);
+
+    /// Get directories which are currently being scanned
+    std::list <std::string> getScanDirectories() const;
 
 private:
+    void stopAndDeleteScanners();
+
     ScannerManager(const ScannerManager&);
     ScannerManager& operator= (const ScannerManager&);
 
-    std::list <Scanner> scanners_;
+    std::list <std::string> scan_directories_;
+    std::list <Scanner*> scanners_;
 };
 
 }
