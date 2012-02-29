7d6e226d97a14ea4a11b3f24c1d509c5c176f914
Your Name <mikatammi@gmail.com>
2012-02-29 23:04:27 +0200

    Added unit test for Settings-class

---
+++ tests/test_settings.cpp
@@ -0,0 +1,48 @@
+#include "Settings.hpp"
+
+#define BOOST_TEST_MODULE Settings test
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( SettingsTest )
+{
+    deck::Settings s0, s1, s2;
+
+    s0.init(false);
+    s1.init(true);
+    s2.init(false);
+    const std::string tmpconfig("/tmp/tmpAQWERT.bin");
+    std::string str("aqwerth54eg/ge21/qwefsdf/ERDGF");
+    std::set <std::string> strset;
+    strset.insert(str);
+
+    s0.setDatabaseDirectories(strset);
+    s0.save(tmpconfig);
+
+    s1.load(tmpconfig);
+    s1.save(tmpconfig);
+
+    s2.load(tmpconfig);
+
+    BOOST_CHECK_EQUAL(*(s0.getDatabaseDirectories().begin()), str);
+    BOOST_CHECK_EQUAL(*(s0.getDatabaseDirectories().begin()),
+                      *(s1.getDatabaseDirectories().begin()));
+    BOOST_CHECK_EQUAL(*(s1.getDatabaseDirectories().begin()),
+                      *(s2.getDatabaseDirectories().begin()));
+
+    s0.load(tmpconfig);
+    BOOST_CHECK_EQUAL(*(s0.getDatabaseDirectories().begin()), str);
+
+    s0.init(true);
+    s0.load(tmpconfig);
+    BOOST_CHECK_EQUAL(*(s0.getDatabaseDirectories().begin()), str);
+
+    s0.clear();
+    BOOST_CHECK_EQUAL(s0.getDatabaseDirectories().size(), 0);
+
+    s0.load(tmpconfig);
+    BOOST_CHECK_EQUAL(*(s0.getDatabaseDirectories().begin()), str);
+
+    BOOST_CHECK_GT(s0.getConfigDefaultLocation().size(), 0);
+    BOOST_CHECK_GT(s0.getConfigDir().size(), 0);
+    BOOST_CHECK_GT(s0.getHomeDir().size(), 0);
+}
