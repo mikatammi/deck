6c98a7c1873f4bfdf0aff202e3b7b2c16f84220b
Mika Tammi <mikatammi@gmail.com>
2012-08-29 01:22:13 +0300

    Added some functions and data containers to Database class

--- src/libdeck/db/Database.cpp
+++ src/libdeck/db/Database.cpp
@@ -1,14 +1,46 @@
 #include "Database.hpp"
+#include "Database.pb.h"
+#include "../Settings.hpp"
 
 namespace deck {
 namespace db {
 
-Database::Database()
+Database::Database() :
+    artists_(),
+    albums_(),
+    tracks_(),
+    readonly_(false)
 {
 }
 
 Database::~Database()
 {
+}
+
+void Database::init(const std::string &filename)
+{
+    // TODO
+}
+
+bool Database::load(const std::string &filename, bool readonly)
+{
+    // TODO
+    readonly_ = readonly;
+
+    // Return false until implemented
+    return false;
+}
+
+bool Database::save()
+{
+    // TODO
+    // Return false until implemented
+    return false;
+}
+
+std::string Database::getDatabaseDir()
+{
+    return deck::Settings::getConfigDir() + "/db";
 }
 
 }
--- src/libdeck/db/Database.hpp
+++ src/libdeck/db/Database.hpp
@@ -1,6 +1,13 @@
 #ifndef DATABASE_HPP
 #define DATABASE_HPP
 
+#include "Artist.hpp"
+#include "Album.hpp"
+#include "Track.hpp"
+
+#include <vector>
+#include <string>
+
 namespace deck {
 namespace db {
 
@@ -10,9 +17,32 @@ public:
     explicit Database();
     ~Database();
 
+    /// Initialize a new database
+    void init(const std::string& filename);
+
+    /// Load database from file
+    /// @param filename Filename of database file.
+    /// @param readonly Whether to open this database file read only.
+    /// @return True on success, False if not
+    bool load(const std::string& filename, bool readonly = false);
+
+    /// Save database to file
+    /// @return True on success, False if not
+    bool save();
+
+    /// Get database directory
+    /// @return Database directory path in string
+    static std::string getDatabaseDir();
+
 private:
     Database(const Database&);
     Database& operator= (const Database &);
+
+    std::vector <Artist> artists_;
+    std::vector <Album> albums_;
+    std::vector <Track> tracks_;
+
+    bool readonly_;
 };
 
 }
